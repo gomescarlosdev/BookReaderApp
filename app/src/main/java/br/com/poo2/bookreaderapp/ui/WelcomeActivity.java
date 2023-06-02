912a2f6d907e2bd2f300f079e8eca1a2bbe609ab
gomescarlosdev <carlos.henrique.pro@outlook.com>
2023-06-01 23:25:35 -0300

    feat:(init): :sparkles: commit inicial

---
+++ app/src/main/java/br/com/poo2/bookreaderapp/ui/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package br.com.poo2.bookreaderapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
